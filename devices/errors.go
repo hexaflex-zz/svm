307b9306b29618b67a5d237b8d0e32d3d6b9ae42
hexaflex <code@hexaflex.nu>
2020-08-28 13:40:33 +0200

    Fixes some linter warnings.

--- devices/errors.go
+++ devices/errors.go
@@ -5,10 +5,12 @@ import "strings"
 // ErrorSet defines a list of one or more errors and is itself an error.
 type ErrorSet []error
 
+// Len returns the number of errors in the error set.
 func (e ErrorSet) Len() int {
 	return len(e)
 }
 
+// Append adds errors to the set.
 func (e *ErrorSet) Append(args ...error) {
 	*e = append(*e, args...)
 }
